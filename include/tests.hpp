febc42a2549af80801a07777dd9d298ab4c3e297
Shayne <114479349+liuxuan1111@users.noreply.github.com>
2026-01-01 17:45:54 +0800

    Update tests.hpp

--- include/tests.hpp
+++ include/tests.hpp
@@ -6,15 +6,16 @@ namespace cltvt
 
     void test_multiplier_V_bounds();
 
-    void test_vt_volatility(const size_t num_samples = 50000);
+    void test_vt_volatility(const size_t num_samples = 100000);
 
-    void test_vt_volatility_simultaneous_limit(const size_t num_samples = 50000);
+    void test_vt_volatility_simultaneous_limit(const size_t num_samples = 100000);
 
-    void test_vt_volatility_limit_along_path_1(const size_t num_samples = 50000);
+    void test_vt_volatility_limit_along_path_1(const size_t num_samples = 100000);
 
-    void test_vt_volatility_limit_along_path_2(const size_t num_samples = 50000);
+    void test_vt_volatility_limit_along_path_2(const size_t num_samples = 100000);
 
-    void test_vt_pricing(const size_t num_samples = 50000);
+    void test_vt_pricing(const size_t num_samples = 100000);
 
-    void test_vt_vega(const size_t num_samples = 50000);
+    void test_vt_vega(const size_t num_samples = 100000);
+
 }
